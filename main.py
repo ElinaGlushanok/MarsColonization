aa68113a0822cd0ed7cd9a7a655be162f60ceca2
Elina <elinaglushanok@gmail.com>
2024-03-16 19:06:36 +0300

    Results added

--- main.py
+++ main.py
@@ -206,6 +206,27 @@ def choice(planet_name):
                           </body>
                         </html>'''
 
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet"
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                                crossorigin="anonymous">
+                        <title>Колонизация</title>
+                          </head>
+                          <body>
+                            <h1 class="mars">Результаты отбора</h1>
+                            <h2>Претендент на участие в миссии: {nickname}</h2>
+                            <div class="alert alert-warning" role="alert">Ваш рейтинг после {level} этапа отбора:</div>
+                            <div class="alert alert-success" role="alert">{rating}</div> 
+                            <div class="alert alert-danger" role="alert">Удачи в следующих этапах!</div>                          
+                          </body>
+                        </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
